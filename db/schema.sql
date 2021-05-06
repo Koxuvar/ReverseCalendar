aeb90502cc7436c1e236b02af1f8cb6f37fce019
emilyalv <emilyaalv@gmail.com>
2021-05-06 09:16:19 -0500

    schema, note file if we want to reference, update to user.js model to match schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
-DROP DATABASE IF EXISTS Reverse_Calender;
+DROP DATABASE IF EXISTS reverse_calendarDB;
+CREATE DATABASE reverse_calendarDB;
 
-CREATE DATABASE Reverse_Calender;
+
+    CREATE TABLE users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(30),
+        password VARCHAR(30),
+    )
+
+    CREATE TABLE dailyCheck (
+        category_id VARCHAR(30)
+        day DATE,
+        user_id INT
+        FOREIGN KEY (user_id) REFERENCES user(id)
+    )
+
+   CREATE TABLE category (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(30),
+        color INT,
+        user_id INT
+        FOREIGN KEY (user_id) REFERENCES user(id)
+    );
