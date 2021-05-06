36670a8b57f7a7bf93f089d7e84867fa04dd989a
MMVHome <connortsullivan1@gmail.com>
2021-05-06 00:35:07 -0500

    homepage styling fixes

--- public/js/index.js
+++ public/js/index.js
@@ -4,17 +4,17 @@ $('#login-form-button').click(e =>
         e.preventDefault();
         $('.signup').css('opacity', '0');
         $('.login').css('opacity', '1');
-        console.log('ello')
     });
     
 $('#signup-form-button').click(e =>
     {
         e.preventDefault();
-        $('.signup').css('opacity', '1');
         $('.login').css('opacity', '0');
+        $('.signup').css('opacity', '1');
     });    
 
 $('#dark-mode-switch').click((e) =>
     {
         $('html, body').toggleClass('bg-dark text-lite');
+        $('button').toggleClass('btn-dark');
     });
