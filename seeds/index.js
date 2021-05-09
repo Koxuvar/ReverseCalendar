4825335e392a94a2e9a11fbf18c0dbff30ba8101
emilyalv <emilyaalv@gmail.com>
2021-05-09 12:21:40 -0500

    updated Models with cascade delete and propercased names, created seed files, updated seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,18 @@
 const sequelize = require('../config/connection');
-const seedDays = require('./daysData');
+// const seedDays = require('./daysData');
+const userSeedData = require('./userSeedData.json');
+const catagorySeedData = require('./catagorySeedData.json');
+dailyCheckSeedData = require('./dailyCheckSeedData.json');
+const { User, Catagory, DailyCheck } = require('../models');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+  const users = await User.bulkCreate(userSeedData);
+  const catagorys = await Catagory.bulkCreate(catagorySeedData);
+  const dailyChecks = await DailyCheck.bulkCreate(dailyCheckSeedData);
 
-  await seedDays();
+
+  // await seedDays();
 
   process.exit(0);
 };
